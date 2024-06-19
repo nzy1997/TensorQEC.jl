dcd4a6facc79d1357ad3da812b021bbe19b4563a
nzy <nzy@nzy-3.local>
2024-06-20 00:14:20 +0800

    save

--- _learn/learn_OME.jl
+++
@@ -1,6 +0,0 @@
-using OMEinsum
-
-a, b = randn(2, 2), randn(2, 2)
-
-c = ein"ij,jk -> ik"(a,b)
-@ein c[i,k] := a[i,j] * b[j,k]
--- _learn/learn_tensorinference.jl
+++
@@ -1,5 +0,0 @@
-using TensorInference
-
-model = read_model_file(pkgdir(TensorInference, "examples", "asia-network", "model.uai"))
-
-tn = TensorNetworkModel(model)
