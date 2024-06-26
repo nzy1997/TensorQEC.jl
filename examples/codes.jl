e9e61eb986a288a73fe294dee651687ffe9afbad
nzy <nzy@nzy-3.local>
2024-06-26 19:31:14 +0800

    up docs

--- examples/codes.jl
+++ examples/codes.jl
@@ -3,4 +3,15 @@
 using TensorQEC
 
 # ## Toric Code
-# The toric code is a 2D topological code. We can 
+# The Toric code is a 2D topological code. We can define a toric code instance  by `ToricCode(m, n)`, where `m` and `n` are the number of rows and columns of the toric code.
+tc = ToricCode(2, 3)
+
+# The toric code has two types of stabilizers: X stabilizers and Z stabilizers. X stabilizers are plaquettes of the lattice, and Z stabilizers are vertices of the lattice. We can get the stabilizers of the toric code by
+st = stabilizers(tc)
+
+# ## Surface Code
+# The surface code is a 2D topological code. Similarly to Toric code, we can define a surface code instance by `SurfaceCode(m, n)` and get the stabilizers of the surface code by `stabilizers(sc)`.
+sc = SurfaceCode(3, 3)
+st = stabilizers(sc)
+
+# ## 
