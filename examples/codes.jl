3b3eaa368d5b426555d11ff84390a60d55e1ae27
nzy <nzy@nzy-3.local>
2024-06-28 17:09:43 +0800

    add code ref

--- examples/codes.jl
+++ examples/codes.jl
@@ -3,7 +3,7 @@
 using TensorQEC
 
 # ## Toric Code
-# The Toric code is a 2D topological code. We can define a Toric code instance by [`ToricCode`](@ref).
+# The Toric code is a 2D topological code[^Kitaev]. We can define a Toric code instance by [`ToricCode`](@ref).
 tc = ToricCode(2, 3)
 # Here is a schematic diagram of 2*3 Toric code:
 # ![](../images/toric.svg)
@@ -14,27 +14,37 @@ st = stabilizers(tc)
 # Note the order of pauli strings is following the big-endian convention. For example, the Pauli string XYZ means $X_1Y_2Z_3$.
 
 # ## Surface Code
-# The surface code is a 2D topological code. Similarly to Toric code, we can define a surface code instance by [`SurfaceCode`](@ref) and get the stabilizers of the surface code by [`stabilizers`](@ref).
+# The surface code is a 2D topological code[^Kitaev]. Similarly to Toric code, we can define a surface code instance by [`SurfaceCode`](@ref) and get the stabilizers of the surface code by [`stabilizers`](@ref).
 sc = SurfaceCode(3, 3)
 st = stabilizers(sc)
 # Here is a schematic diagram of 3*3 surface code:
 # ![](../images/surface.svg)
 
 # ## Shor Code
-# The [[9,1,3]] Shor code functions by concatenating each qubit of a phase-flip with a bit-flip repetition code, allowing it to correct both types of errors at the same time. We can define a Shor code instance by [`ShorCode`](@ref).
+# The [[9,1,3]] Shor code[^Shor] functions by concatenating each qubit of a phase-flip with a bit-flip repetition code, allowing it to correct both types of errors at the same time. We can define a Shor code instance by [`ShorCode`](@ref).
 shor = ShorCode()
 st = stabilizers(shor)
 
 # ## Steane Code
-# The [[7,1,3]] Steane is constructed using the classical binary [7,4,3] Hamming code. We can define a Steane code instance by [`SteaneCode`](@ref).
+# The [[7,1,3]] Steane is constructed using the classical binary [7,4,3] Hamming code[^Steane]. We can define a Steane code instance by [`SteaneCode`](@ref).
 
 # ![](../images/steane.svg)
 steane = SteaneCode()
 st = stabilizers(steane)
 
 # ## [[8,3,2]] Code
-# The [[8,3,2]] CSS code is the smallest non-trivial 3D color code. We can define a CSS [[8,3,2]] code instance by [`Code832`](@ref).
+# The [[8,3,2]] CSS code is the smallest non-trivial 3D color code[^Menendez][^Code832]. We can define a CSS [[8,3,2]] code instance by [`Code832`](@ref).
 
 # ![](../images/code832.svg)
 code832 = Code832()
 st = stabilizers(code832)
+
+# [^Kitaev]: Kitaev, A. Y. Fault-Tolerant Quantum Computation by Anyons. Annals of Physics 2003, 303 (1), 2–30. https://doi.org/10.1016/S0003-4916(02)00018-0.
+
+# [^Menendez]: Menendez, D. H.; Ray, A.; Vasmer, M. Implementing Fault-Tolerant Non-Clifford Gates Using the [[8,3,2]] Color Code. arXiv September 15, 2023. http://arxiv.org/abs/2309.08663.
+
+# [^Code832]: [THE SMALLEST INTERESTING COLOUR CODE](https://earltcampbell.com/2016/09/26/the-smallest-interesting-colour-code/)
+
+# [^Shor]: Shor, P. W. (1995). Scheme for reducing decoherence in quantum computer memory. Physical Review A, 52(4), R2493–R2496. https://doi.org/10.1103/PhysRevA.52.R2493
+
+# [^Steane]: Steane, A. (1997). Multiple-particle interference and quantum error correction. Proceedings of the Royal Society of London. Series A: Mathematical, Physical and Engineering Sciences, 452(1954), 2551–2577. https://doi.org/10.1098/rspa.1996.0136
