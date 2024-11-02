e932ea10571ccf81c760e7d1a6a206549c21bd37
nzy <nzy@nzy-3.local>
2024-11-02 10:25:51 +0800

    fix simulation test

--- test/simulation.jl
+++ test/simulation.jl
@@ -126,7 +126,7 @@ end
 @testset "coherent_error_unitary" begin
     u = rand_unitary(16)
     inf = Float64[]
-    u2 = coherent_error_unitary(u, 1e-2;cache = inf)
+    u2 = coherent_error_unitary(u, 1e-4;cache = inf)
     @test u2'*u2 ≈ I
     @test inf[1] < 1e-2
     @test inf[1] > 0
@@ -143,13 +143,13 @@ end
 @testset "error_quantum_circuit" begin
     qc = chain(3, put(3, 1 => X), put(3, 2 => H), put(3, 3 => Z), control(3, 2, 3 => X), control(3, (1,3), 2 => Z), control(3, 1, 3 => Z))
     qc, srs = ein_circ(qc, QCInfo([], [], 3))
-    qce = error_quantum_circuit(qc ,1e-5) 
+    qce = error_quantum_circuit(qc ,1e-7) 
     @test isapprox(1-abs(tr(mat(qc)'*mat(qce)))/2^6,0; atol=1e-5)
 end
 
 @testset "error_quantum_circuit_pair_replace" begin
     qc = chain(3, put(3, 1 => X), put(3, 2 => H), put(3, 3 => Z), control(3, 2, 3 => X), control(3, (1,3), 2 => Z), control(3, 1, 3 => Z))
     qc, srs = ein_circ(qc, QCInfo([], [], 3))
-    qce,vec = error_quantum_circuit_pair_replace(qc ,1e-5) 
+    qce,vec = error_quantum_circuit_pair_replace(qc ,1e-7) 
     @test isapprox(1-abs(tr(mat(qc)'*mat(qce)))/2^6,0; atol=1e-5)
 end
