2f66205c07aced308707b74d84709aedcca96113
Zhongyi <zni573@connect.hkust-gz.edu.cn>
2024-12-05 14:42:34 +0800

    code 422

--- src/TensorQEC.jl
+++ src/TensorQEC.jl
@@ -34,7 +34,7 @@ export PauliString, PauliGroup, isanticommute,paulistring
 export syndrome_inference, measure_syndrome!,correction_pauli_string, generate_syndrome_dict,pauli_string_map_iter, inference, transformed_sydrome_dict
 
 # codes 
-export toric_code, stabilizers,ToricCode, SurfaceCode, ShorCode,SteaneCode,Code832
+export toric_code, stabilizers,ToricCode, SurfaceCode, ShorCode,SteaneCode,Code832, Code422
 
 # encoder
 export CSSBimatrix,syndrome_transform, encode_stabilizers,place_qubits
