078ea90f20d3e5d74f0dec83c0b65dfc56489b4f
Zhongyi <zni573@connect.hkust-gz.edu.cn>
2025-02-20 15:53:12 +0800

    code distance

--- src/code_distance.jl
+++ src/code_distance.jl
@@ -28,3 +28,82 @@ function row_echelon_form(H::Matrix{Bool})
     gaussian_elimination!(bimat,1:size(H, 1), 0, 0;allow_col_operation = false)
     return bimat.matrix
 end
+
+function null_space(H::Matrix{Bool})
+    reH = row_echelon_form(H)
+    m,n = size(H)
+
+    local x
+    pivots = [x for i in 1:m if begin x = findfirst(!iszero, reH[i,:]); !isnothing(x) end]
+
+    rank = length(pivots)
+    null_space = zeros(Bool, n-rank, n)
+
+    row_count = 0
+    for j in 1:n
+        if j ∉ pivots
+            row_count += 1
+            null_space[row_count, j] = 1
+            for i in 1:length(pivots)
+                reH[i,j] && (null_space[row_count, pivots[i]] = true)
+            end
+        end
+    end
+    return null_space
+end
+
+function logical_oprator(Hx::Matrix{Bool}, Hz::Matrix{Bool})
+    kerHx = null_space(Hx)
+
+    H = [Hz; kerHx]
+    bimat = gaussian_elimination!(SimpleBimatrix(H), 1:size(H, 1), 0, 0)
+
+    lz = bimat.matrix[1+size(Hz, 1):end, :]
+
+    lz = lz[any.(!iszero, eachrow(lz)), :]
+    lz[:,bimat.ordering] = lz
+    return lz
+end
+
+function logical_oprator(tanner::CSSTannerGraph)
+    lz = logical_oprator([a.x for a in tanner.stgx.H], [a.x for a in tanner.stgz.H])
+    lx = logical_oprator([a.x for a in tanner.stgz.H], [a.x for a in tanner.stgx.H])
+    return lx, lz
+end
+
+function code_distance(Hz::Matrix{Int},lz::Matrix{Int}; verbose = false)
+    # H : m x n
+
+    m,n = size(Hz)
+    num_lz = size(lz, 1)
+    model = Model(HiGHS.Optimizer)
+    !verbose && set_silent(model)
+
+    @variable(model, 0 <= z[i = 1:n] <= 1, Int)
+    @variable(model, 0 <= k[i = 1:m], Int)
+    @variable(model, 0 <= l[i = 1:num_lz], Int)
+    @variable(model, 0 <= r[i = 1:num_lz] <= 1, Int)
+    
+    for i in 1:m
+        @constraint(model, sum(z[j] for j in 1:n if Hz[i,j] == 1) == 2 * k[i])
+    end
+    @constraint(model, sum(z[j] for j in 1:n) >= 1)
+
+
+    for i in 1:num_lz
+        @constraint(model, sum(z[j] for j in 1:n if lz[i,j] == 1) == 2*l[i] + r[i])
+    end
+    @constraint(model, sum(r[i] for i in 1:num_lz) >= 1)
+
+    @objective(model, Min, sum(z[j] for j in 1:n))
+    optimize!(model)
+    @assert is_solved_and_feasible(model) "The problem is infeasible!"
+    return  objective_value(model)
+end
+
+function code_distance(tanner::CSSTannerGraph)
+    lx,lz = logical_oprator(tanner)
+    dx = code_distance(Int.(tanner.stgz.H), Int.(lz))
+    dz = code_distance(Int.(tanner.stgx.H), Int.(lx))
+    return min(dx,dz)
+end
--- test/code_distance.jl
+++ test/code_distance.jl
@@ -1,6 +1,6 @@
 using Test
 using TensorQEC
-using TensorQEC: row_echelon_form
+using TensorQEC: row_echelon_form,null_space,logical_oprator
 
 @testset "classical_code_distance" begin
     H = [0 0 0 1 1 1 1;0 1 1 0 0 1 1; 1 0 1 0 1 0 1]
@@ -34,4 +34,41 @@ end
     0  0  0  0  0  0  0  1  1  0;
     0  0  0  0  0  0  0  0  0  1;
     0  0  0  0  0  0  0  0  0  0]
+end
+
+@testset "null_space" begin
+    H = Bool[0 0 0 1 1 1 1;0 1 1 0 0 1 1; 1 0 1 0 1 0 1]
+    kerH = null_space(H)
+    @test size(kerH) == (4,7)
+    for i in 1:4
+        @test Mod2.(H) * Mod2.(kerH[i,:]) == zeros(Mod2, 3)
+    end
+
+    local x
+    pivots = [x for i in 1:4 if begin x = findfirst(!iszero, kerH[i,:]); !isnothing(x) end]
+    rank = length(pivots)
+    @test rank == 4
+end
+@testset "logical_oprator" begin
+    st = stabilizers(SurfaceCode(3, 3))
+    tannerxz = CSSTannerGraph(st)
+    lx,lz = logical_oprator(tannerxz)
+    @test lx == Bool[0 0 0 0 0 0 1 1 1]
+    @test lz == Bool[1 0 0 0 1 0 0 0 1]
+end
+
+@testset "code_distance" begin
+    st = stabilizers(SurfaceCode(3, 3))
+    tannerxz = CSSTannerGraph(st)
+    lx,lz = logical_oprator(tannerxz)
+    @test code_distance(Int.(tannerxz.stgz.H),Int.(lz)) == 3
+    @test code_distance(Int.(tannerxz.stgx.H),Int.(lx)) == 3
+
+    st = stabilizers(SurfaceCode(5, 5))
+    tannerxz = CSSTannerGraph(st)
+    @test code_distance(tannerxz) == 5
+
+    st = stabilizers(Code422())
+    tannerxz = CSSTannerGraph(st)
+    @test code_distance(tannerxz) == 2
 end
