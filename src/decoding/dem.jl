5f58cbced8547a25d8b3e5d247accc3a91fb9d78
nzy <nzy@nzy-3.local>
2025-08-06 17:35:48 +0800

    viz dem

--- src/decoding/dem.jl
+++ src/decoding/dem.jl
@@ -1,6 +1,22 @@
 struct DetectorErrorModel
     error_rates::Vector{Float64}
     flipped_detectors::Vector{Vector{Int}}
+    detector_list::Vector{Int}
+    logical_list::Vector{Int}
+end
+
+# visualization
+Base.show(io::IO, ::MIME"text/plain", dem::DetectorErrorModel) = show(io, dem)
+function Base.show(io::IO, dem::DetectorErrorModel)
+    fd = Vector{Vector{Int}}()
+    fl = Vector{Vector{Int}}()
+    for i in 1:length(dem.error_rates)
+        push!(fd, dem.flipped_detectors[i] ∩ dem.detector_list)
+        push!(fl, dem.flipped_detectors[i] ∩ dem.logical_list)
+    end
+	header = ["Error", "Detectors", "Logicals"]
+	pt = pretty_table(io, hcat(dem.error_rates, fd, fl); header)
+	return nothing
 end
 
 function detector_error_model(qc::ChainBlock)
@@ -8,33 +24,42 @@ function detector_error_model(qc::ChainBlock)
     cqc = compile_clifford_circuit(qc)
     @assert length(cqc.sequence) == length(qc)
     num_qubits = nqubits(qc)
-    ers = Vector{Float64}()
-    fds = Vector{Vector{Int}}()
+    dl = Vector{Int}()
+    ll = Vector{Int}()
+    detector_error_map = Dict{Vector{Int}, Float64}()
     for (i, gate) in enumerate(qc)
         if gate isa PutBlock && gate.content isa MixedUnitaryChannel
             for (j, op) in enumerate(gate.content.operators)
                 if op == I2
                 elseif op == X
                     ps = PauliString(num_qubits,gate.locs[1]=>Pauli(1))
-                    push!(fds,forward_analysis(ps, cqc, i, qc))
-                    push!(ers, gate.content.probs[j])
+                    push_to_dict!(detector_error_map, forward_analysis(ps, cqc, i, qc), gate.content.probs[j])
                 elseif op == Z
                     ps = PauliString(num_qubits,gate.locs[1]=>Pauli(3))
-                    forward_analysis(ps, cqc, i, qc)
+                    push_to_dict!(detector_error_map, forward_analysis(ps, cqc, i, qc), gate.content.probs[j])
                 end
             end
         elseif gate isa PutBlock && gate.content isa DepolarizingChannel
             @assert gate.content.n == 1 "Depolarizing channel is not supported for multiple qubits"
             ps = PauliString(num_qubits,gate.locs[1]=>Pauli(1))
-            push!(fds,forward_analysis(ps, cqc, i, qc))
-            push!(ers, gate.content.p*2/3)
+            push_to_dict!(detector_error_map, forward_analysis(ps, cqc, i, qc), gate.content.p*2/3)
 
             ps = PauliString(num_qubits,gate.locs[1]=>Pauli(3))
-            push!(fds,forward_analysis(ps, cqc, i, qc))
-            push!(ers, gate.content.p*2/3)
+            push_to_dict!(detector_error_map, forward_analysis(ps, cqc, i, qc), gate.content.p*2/3)
+        elseif gate isa PutBlock && gate.content isa DetectorBlock && gate.content.detector_type == 0 
+            push!(dl, gate.content.num)
+        elseif gate isa PutBlock && gate.content isa DetectorBlock && gate.content.detector_type == 1
+            push!(ll, gate.content.num)
         end
     end
-    return DetectorErrorModel(ers,fds)
+    ers = Float64[]
+    fds = Vector{Int}[]
+    
+    for (detectors, error_rate) in detector_error_map
+        push!(ers, error_rate)
+        push!(fds, detectors)
+    end
+    return DetectorErrorModel(ers,fds,dl,ll)
 end
 
 function forward_analysis(ps::PauliString, cqc::CompiledCliffordCircuit, i::Int, qc::ChainBlock)
@@ -86,8 +111,17 @@ function forward_analysis(ps::PauliString, cqc::CompiledCliffordCircuit, i::Int,
     return flipped_detectors
 end
 
-function postprocess(ers,fds)
-
+function push_to_dict!(detector_error_map::Dict{Vector{Int}, Float64}, detectors::Vector{Int}, error_rate::Float64)
+    if isempty(detectors)
+        return detector_error_map
+    end
+    sorted_detectors = sort(detectors)
+    if haskey(detector_error_map, sorted_detectors)
+        detector_error_map[sorted_detectors] = (1-detector_error_map[sorted_detectors]) * error_rate + detector_error_map[sorted_detectors] * (1- error_rate)
+    else
+        detector_error_map[sorted_detectors] = error_rate
+    end
+    return detector_error_map
 end
 
 # TODO:
--- test/decoding/dem.jl
+++ test/decoding/dem.jl
@@ -4,7 +4,7 @@ using TensorQEC
 @testset "detector error model" begin
     circuit_str = """
     X_ERROR(0.2) 0 1
-    # DEPOLARIZE1(0.0001) 0 1
+    DEPOLARIZE1(0.03) 0 1
 
     CX 0 1
 
@@ -12,15 +12,17 @@ using TensorQEC
     DETECTOR rec[-1]
     DETECTOR rec[-2]
     DETECTOR rec[-1] rec[-2]
+    OBSERVABLE_INCLUDE(0) rec[-1] rec[-2]
     """
     qc = TensorQEC.parse_stim_string(circuit_str,2)
 
     dem = TensorQEC.detector_error_model(qc)
-    @test dem.error_rates == [0.2,0.2]
-    @test dem.flipped_detectors == [[1,2],[1,3]]
+    println(dem)
+    @test dem.error_rates ≈ [0.212,0.212] atol=1e-10
+    @test dem.flipped_detectors == [[1,3,4],[1,2]]
 end
 
-@testset "detector error model" begin
+@testset "detector error model with reset" begin
     circuit_str ="""
     X_ERROR(0.2) 0 1
 
@@ -32,7 +34,7 @@ end
     R 1
 
     X_ERROR(0.2) 0 1
-    CX 0 1
+    CX 1 0
 
     M 0 1
     DETECTOR rec[-1] rec[-2]   
@@ -40,26 +42,17 @@ end
     qc = TensorQEC.parse_stim_string(circuit_str,2)
 
     dem = TensorQEC.detector_error_model(qc)
-    @test dem.error_rates == [0.2, 0.2, 0.2, 0.2]
-    @test dem.flipped_detectors == [Int64[], [1], Int64[], [2]]
+    println(dem)
+    @test dem.error_rates ≈ [0.2,0.32] atol=1e-10
+    @test dem.flipped_detectors == [[1],[2]]
 end
 
 @testset "detector error model" begin
     circuit_str = """
-    QUBIT_COORDS(0, 0) 0
-    QUBIT_COORDS(1, 0) 1
-    QUBIT_COORDS(2, 0) 2
-    QUBIT_COORDS(3, 0) 3
-    QUBIT_COORDS(0.5, 1) 4
-    QUBIT_COORDS(1.5, 1) 5
-    QUBIT_COORDS(2.5, 1) 6
-    QUBIT_COORDS(1, 2) 7
-    QUBIT_COORDS(2, 2) 8
-    QUBIT_COORDS(1.5, 3) 9
     R 0 1 2 3 4 5 6 7 8 9
     REPEAT 2 {
         TICK
-        DEPOLARIZE1(0.04) 0 1 3 5 6 7 9
+        DEPOLARIZE1(0.03) 0 1 3 5 6 7 9
         C_XYZ 0 1 3 5 6 7 9
         TICK
         CX 5 4 3 2
@@ -74,13 +67,11 @@ end
         TICK
         CX 0 4 5 8
         TICK
-        X_ERROR(0.01) 2 4 8
         MR 2 4 8
     }
     DETECTOR(2, 0, 0) rec[-3] rec[-6]
     DETECTOR(0.5, 1, 0) rec[-2] rec[-5]
     DETECTOR(2, 2, 0) rec[-1] rec[-4]
-    X_ERROR(0.01) 0 1 3 5 6 7 9
     MY 0 1 3 5 6 7 9
     DETECTOR(2, 0, 1) rec[-3] rec[-4] rec[-5] rec[-6] rec[-10] rec[-13]
     DETECTOR(0.5, 1, 1) rec[-2] rec[-4] rec[-6] rec[-7] rec[-9] rec[-12]
@@ -90,7 +81,6 @@ end
     qc = TensorQEC.parse_stim_string(circuit_str,10)
 
     dem = TensorQEC.detector_error_model(qc)
-    @test dem.error_rates == [0.2,0.2]
-    @test dem.flipped_detectors == [[1,2],[1,3]]
+    println(dem)
 end
 
